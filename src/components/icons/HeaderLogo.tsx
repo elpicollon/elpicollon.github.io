964ebb07ce6f799b89923cfb3d67bdc430ac71ca
elpicollon <rodrigopicollo@outlook.com>
2026-01-12 10:37:07 -0300

    Logo adequations

---
+++ src/components/icons/HeaderLogo.tsx
@@ -0,0 +1,54 @@
+import svgPaths from "./svg-2onjs0o2af";
+
+function Logo() {
+  return (
+    <div className="absolute inset-[4.33%_1.39%_4.67%_1.39%]" data-name="Logo">
+      <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 700 273">
+        <g id="Logo">
+          <g id="Stroke">
+            <path clipRule="evenodd" d={svgPaths.p242e4700} fill="var(--fill-0, #09090B)" fillRule="evenodd" />
+            <path clipRule="evenodd" d={svgPaths.p20173200} fill="var(--fill-0, #09090B)" fillRule="evenodd" />
+            <path clipRule="evenodd" d={svgPaths.p18609d00} fill="var(--fill-0, #09090B)" fillRule="evenodd" />
+            <path clipRule="evenodd" d={svgPaths.p20e43200} fill="var(--fill-0, #09090B)" fillRule="evenodd" />
+            <path clipRule="evenodd" d={svgPaths.p3883ac00} fill="var(--fill-0, #09090B)" fillRule="evenodd" />
+            <path clipRule="evenodd" d={svgPaths.p16124d80} fill="var(--fill-0, #09090B)" fillRule="evenodd" />
+            <path d={svgPaths.p3d9f5e70} fill="var(--fill-0, #09090B)" />
+            <path d={svgPaths.p11837180} fill="var(--fill-0, #09090B)" />
+            <path d={svgPaths.p356aee00} fill="var(--fill-0, #09090B)" />
+            <path d={svgPaths.p18ccfec0} fill="var(--fill-0, #09090B)" />
+            <path clipRule="evenodd" d={svgPaths.p33f56700} fill="var(--fill-0, #09090B)" fillRule="evenodd" />
+            <path d={svgPaths.pb770120} fill="var(--fill-0, #09090B)" />
+            <path clipRule="evenodd" d={svgPaths.p36975700} fill="var(--fill-0, #09090B)" fillRule="evenodd" />
+            <path d={svgPaths.p2ac07580} fill="var(--fill-0, #09090B)" />
+            <path d={svgPaths.p283eb000} fill="var(--fill-0, #09090B)" />
+            <path d={svgPaths.p7cbd900} fill="var(--fill-0, #09090B)" />
+            <path d={svgPaths.p12a8d572} fill="var(--fill-0, #09090B)" />
+            <path d={svgPaths.p14d61180} fill="var(--fill-0, #09090B)" />
+            <path clipRule="evenodd" d={svgPaths.p149bad00} fill="var(--fill-0, #09090B)" fillRule="evenodd" />
+            <path d={svgPaths.p2fe60100} fill="var(--fill-0, #09090B)" />
+            <path d={svgPaths.p34f82a80} fill="var(--fill-0, #09090B)" />
+            <path d={svgPaths.p3cda3900} fill="var(--fill-0, #09090B)" />
+            <path d={svgPaths.p376aa00} fill="var(--fill-0, #09090B)" />
+          </g>
+          <g id="PÃ¬colo">
+            <path clipRule="evenodd" d={svgPaths.p3722f500} fill="var(--fill-0, #2DB354)" fillRule="evenodd" id="O" />
+            <path d={svgPaths.p1dd8ce80} fill="var(--fill-0, #2DB354)" id="L" />
+            <path clipRule="evenodd" d={svgPaths.p267f73f0} fill="var(--fill-0, #2DB354)" fillRule="evenodd" id="O_2" />
+            <path clipRule="evenodd" d={svgPaths.p34900a00} fill="var(--fill-0, #2DB354)" fillRule="evenodd" id="C" />
+            <path clipRule="evenodd" d={svgPaths.p31cd7e80} fill="var(--fill-0, #2DB354)" fillRule="evenodd" id="I" />
+            <path d={svgPaths.p27d34700} fill="var(--fill-0, #905FDA)" id="I_2" />
+            <path clipRule="evenodd" d={svgPaths.p2f29500} fill="var(--fill-0, #2DB354)" fillRule="evenodd" id="P" />
+          </g>
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+export default function PicoloDesignLogo() {
+  return (
+    <div className="relative size-full" data-name="picolo.design-logo">
+      <Logo />
+    </div>
+  );
+}
