1b2e6382625b6b860f192e44f7d590dce1059f6c
elpicollon <rodrigopicollo@outlook.com>
2026-01-13 22:38:09 -0300

    chore: remove arquivos não utilizados da pasta imports (~267KB)

--- src/imports/Container.tsx
+++
@@ -1,3 +0,0 @@
-export default function Container() {
-  return <div className="bg-gradient-to-b from-[rgba(0,0,0,0.2)] opacity-60 size-full to-[#000000] to-[80.288%] via-50% via-[rgba(0,0,0,0.1)]" data-name="Container" />;
-}
