13c26417ef66f01932781dec5f09f16bb3389a3f
elpicollon <rodrigopicollo@outlook.com>
2026-01-14 09:40:29 -0300

    Ajuste de imagens de capa

--- src/components/HorizontalScroll.tsx
+++ src/components/HorizontalScroll.tsx
@@ -11,7 +11,7 @@ const projects = [
     title: 'Transcrições & Insights com IA',
     category: 'UX Design',
     year: '2024',
-    image: '/assets/projects/transcricoes-insights-ia/cover.png',
+    image: '/assets/projects/transcricoes-insights-ia/card-home.png',
     gradient: 'linear-gradient(to bottom, #2F968C, #00463F)',
     link: '/projeto/transcricoes-insights-ia'
   },
